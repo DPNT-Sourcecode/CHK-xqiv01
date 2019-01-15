0668b51d16c41c9db87f267fbe5046a39b1779ed
sbx_user1060 <sbx_user1060@ip-10-129-51-70.eu-west-2.compute.internal>
2019-01-15 16:30:54 +0000

    Tue Jan 15 16:30:54 UTC 2019

--- src/main/java/befaster/solutions/CHK/Basket.java
+++ src/main/java/befaster/solutions/CHK/Basket.java
@@ -7,6 +7,26 @@ public class Basket {
 	int totalD;
 	int totalE;
 	int totalF;
+	int totalG;
+	int totalH;
+	int totalI;
+	int totalJ;
+	int totalK;
+	int totalL;
+	int totalM;
+	int totalN;
+	int totalO;
+	int totalP;
+	int totalQ;
+	int totalR;
+	int totalS;
+	int totalT;
+	int totalU;
+	int totalV;
+	int totalW;
+	int totalX;
+	int totalY;
+	int totalZ;
 	
 	public Basket() {
 		totalA=0;
@@ -37,6 +57,66 @@ public class Basket {
 		case 'F':
 			totalF++;
 			break;
+		case 'G':
+			totalG++;
+			break;
+		case 'H':
+			totalH++;
+			break;
+		case 'I':
+			totalI++;
+			break;
+		case 'J':
+			totalJ++;
+			break;
+		case 'K':
+			totalK++;
+			break;
+		case 'L':
+			totalL++;
+			break;
+		case 'M':
+			totalM++;
+			break;
+		case 'N':
+			totalN++;
+			break;
+		case 'O':
+			totalO++;
+			break;
+		case 'P':
+			totalP++;
+			break;
+		case 'Q':
+			totalQ++;
+			break;
+		case 'R':
+			totalR++;
+			break;
+		case 'S':
+			totalS++;
+			break;
+		case 'T':
+			totalT++;
+			break;
+		case 'U':
+			totalU++;
+			break;
+		case 'V':
+			totalV++;
+			break;
+		case 'W':
+			totalW++;
+			break;
+		case 'X':
+			totalX++;
+			break;
+		case 'Y':
+			totalY++;
+			break;	
+		case 'Z':
+			totalZ++;
+			break;
 		default:
 			throw new Exception();
 		}
@@ -44,3 +124,4 @@ public class Basket {
 	
 	
 }
+
