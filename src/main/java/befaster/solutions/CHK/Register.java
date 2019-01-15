cfd7d6de50f85556052d77271992cb0a408d3f25
sbx_user1060 <sbx_user1060@ip-10-129-51-70.eu-west-2.compute.internal>
2019-01-15 16:24:54 +0000

    Tue Jan 15 16:24:54 UTC 2019

--- src/main/java/befaster/solutions/CHK/Register.java
+++ src/main/java/befaster/solutions/CHK/Register.java
@@ -1,12 +1,7 @@
 package befaster.solutions.CHK;
 
 public class Register {
-	int totalA;
-	int totalB;
-	int totalC;
-	int totalD;
-	int totalE;
-	int totalF;
+	Basket shopping;
 	
 	Item itemA = new Item('A',50);
 	Item itemB = new Item('B',30);
@@ -14,13 +9,9 @@ public class Register {
 	Item itemD = new Item('D',15);
 	Item itemE = new Item('E',40);
 	Item itemF = new Item('F',10);
+	
 	public Register (Basket shopping) {
-		this.totalA = shopping.totalA;
-		this.totalB = shopping.totalB;
-		this.totalC = shopping.totalC;
-		this.totalD = shopping.totalD;
-		this.totalE = shopping.totalE;
-		this.totalF = shopping.totalF;
+		this.shopping = shopping;
 	}
 	
 	// calculations for the total cost for each item
@@ -28,34 +19,35 @@ public class Register {
 			int totalCost = 0;
 			//check for specials of item A in multiples of 5
 			itemA.setSpecial(5, 200);
-			totalCost += itemA.checkSpecialCost(totalA);
-			totalA %= 5;
+			totalCost += itemA.checkSpecialCost(shopping.totalA);
+			shopping.totalA %= 5;
 			//check for specials of item A in multiples of 3
 			itemA.setSpecial(3, 130);
-			totalCost += itemA.checkSpecialCost(totalA);
-			totalA %= 3;
+			totalCost += itemA.checkSpecialCost(shopping.totalA);
+			shopping.totalA %= 3;
 			//check total cost of item A after specials applied
-			totalCost += itemA.checkCost(totalA);
+			totalCost += itemA.checkCost(shopping.totalA);
 			// remove one free B item for every 2 E items
-			totalB -= totalE/2;
-			if (totalB <0) totalB=0;
+			shopping.totalB -= shopping.totalE/2;
+			if (shopping.totalB <0) shopping.totalB=0;
 			//check for specials of item B in multiples of 2
 			itemB.setSpecial(2, 45);
-			totalCost += itemB.checkSpecialCost(totalB);
-			totalB %= 2;
+			totalCost += itemB.checkSpecialCost(shopping.totalB);
+			shopping.totalB %= 2;
 			//check total cost of item B after specials applied
-			totalCost += itemB.checkCost(totalB);
+			totalCost += itemB.checkCost(shopping.totalB);
 			//check total cost of item C after specials applied
-			totalCost += itemC.checkCost(totalC);
+			totalCost += itemC.checkCost(shopping.totalC);
 			//check total cost of item D after specials applied
-			totalCost += itemD.checkCost(totalD);
+			totalCost += itemD.checkCost(shopping.totalD);
 			//check total cost of item E after specials applied
-			totalCost += itemE.checkCost(totalE);
+			totalCost += itemE.checkCost(shopping.totalE);
 			// reduction in totalF for free items
-			totalF -= (totalF/3);
+			shopping.totalF -= (shopping.totalF/3);
 			//check total cost of item F after specials applied
-			totalCost += itemF.checkCost(totalF);
+			totalCost += itemF.checkCost(shopping.totalF);
 			
 			return totalCost;
 		}
 }
+
