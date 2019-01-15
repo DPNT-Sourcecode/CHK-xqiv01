57276ef70f2e5213b97ffd338e49f3bc341cb4d7
sbx_user1059 <sbx_user1059@ip-10-129-227-17.eu-west-2.compute.internal>
2019-01-15 17:19:51 +0000

    Tue Jan 15 17:19:51 UTC 2019

--- src/main/java/befaster/solutions/CHK/AvailableItems.java
+++ src/main/java/befaster/solutions/CHK/AvailableItems.java
@@ -1,5 +1,37 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+
 public class AvailableItems {
+	HashMap itemSet = new HashMap();
+	itemSet.put('A',new Item('A',50));
+	
+	Item itemA = new Item('A',50);
+	Item itemB = new Item('B',30);
+	Item itemC = new Item('C',20);
+	Item itemD = new Item('D',15);
+	Item itemE = new Item('E',40);
+	Item itemF = new Item('F',10);
+	Item itemG = new Item('G',20);
+	Item itemH = new Item('H',10);
+	Item itemI = new Item('I',35);
+	Item itemJ = new Item('J',60);
+	Item itemK = new Item('K',80);
+	Item itemL = new Item('L',90);
+	Item itemM = new Item('M',15);
+	Item itemN = new Item('N',40);
+	Item itemO = new Item('O',10);
+	Item itemP = new Item('P',50);
+	Item itemQ = new Item('Q',30);
+	Item itemR = new Item('R',50);
+	Item itemS = new Item('S',30);
+	Item itemT = new Item('T',20);
+	Item itemU = new Item('U',40);
+	Item itemV = new Item('V',50);
+	Item itemW = new Item('W',20);
+	Item itemX = new Item('X',90);
+	Item itemY = new Item('Y',10);
+	Item itemZ = new Item('Z',50);
 
 }
+
