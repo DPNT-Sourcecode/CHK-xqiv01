72d19ce8c5af87a4070d753951f74ef44e4976c0
sbx_user1059 <sbx_user1059@ip-10-129-211-211.eu-west-2.compute.internal>
2019-01-15 14:15:35 +0000

    Tue Jan 15 14:15:35 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,13 @@
 package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
+import java.io.*;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        //throw new SolutionNotImplementedException();
+    	System.out.println(skus);
+    	return 0;
     }
 }
+
