750ec8839478ec9f21c4e75dcd1953c3910a21e7
sbx_user1060 <sbx_user1060@ip-10-128-217-9.eu-west-2.compute.internal>
2019-01-15 20:19:37 +0000

    Tue Jan 15 20:19:37 UTC 2019

---
+++ src/test/java/befaster/solutions/CHK/SumSolutionTest.java
@@ -0,0 +1,22 @@
+package befaster.solutions.CHK;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SumSolutionTest {
+    private CheckoutSolution chOut;
+
+    @Before
+    public void setUp() {
+
+    	chOut = new CheckoutSolution();
+    }
+
+    @Test
+    public void compute_sum() {
+        assertThat(chOut.checkout("A"), equalTo(50));
+    }
+}
