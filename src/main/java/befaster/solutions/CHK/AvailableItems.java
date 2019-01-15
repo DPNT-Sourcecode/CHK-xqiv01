646417a04ab35263b81e5fa49ccbbcfd8d7f02f3
sbx_user1059 <sbx_user1059@ip-10-129-227-17.eu-west-2.compute.internal>
2019-01-15 18:30:29 +0000

    Tue Jan 15 18:30:29 UTC 2019

--- src/main/java/befaster/solutions/CHK/AvailableItems.java
+++ src/main/java/befaster/solutions/CHK/AvailableItems.java
@@ -15,25 +15,26 @@ public class AvailableItems {
 		itemSet.put('H',new Item('H',10));
 		itemSet.put('I',new Item('I',35));
 		itemSet.put('J',new Item('J',60));
-		itemSet.put('K',new Item('K',80));
+		itemSet.put('K',new Item('K',70));
 		itemSet.put('L',new Item('L',90));
 		itemSet.put('M',new Item('M',15));
 		itemSet.put('N',new Item('N',40));
 		itemSet.put('O',new Item('O',10));
 		itemSet.put('P',new Item('P',50));
-		itemSet.put('Q',new Item('Q',50));
+		itemSet.put('Q',new Item('Q',30));
 		itemSet.put('R',new Item('R',50));
-		itemSet.put('S',new Item('S',50));
-		itemSet.put('T',new Item('T',50));
-		itemSet.put('U',new Item('U',50));
+		itemSet.put('S',new Item('S',20));
+		itemSet.put('T',new Item('T',20));
+		itemSet.put('U',new Item('U',40));
 		itemSet.put('V',new Item('V',50));
-		itemSet.put('W',new Item('W',50));
-		itemSet.put('X',new Item('X',50));
-		itemSet.put('Y',new Item('Y',50));
-		itemSet.put('Z',new Item('Z',50));
+		itemSet.put('W',new Item('W',20));
+		itemSet.put('X',new Item('X',17));
+		itemSet.put('Y',new Item('Y',20));
+		itemSet.put('Z',new Item('Z',21));
 	}
 	
 	public Item getValidItem (char sku) throws Exception{
 		return itemSet.get(sku);
 	}
 }
+
