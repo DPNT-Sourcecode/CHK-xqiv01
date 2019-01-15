f06815a7cf7c30981505aa3e7a6f06d5df854fbf
sbx_user1059 <sbx_user1059@ip-10-129-211-211.eu-west-2.compute.internal>
2019-01-15 14:45:34 +0000

    Tue Jan 15 14:45:34 UTC 2019

--- src/main/java/befaster/solutions/CHK/Item.java
+++ src/main/java/befaster/solutions/CHK/Item.java
@@ -1,5 +1,33 @@
 package befaster.solutions.CHK;
 
 public class Item {
-
+	private char itemSKU;
+	private int unitPrice;
+	private int qtyForSpecial;
+	private int priceForSpecial;
+	boolean specialAvailable;
+	
+	public Item (char sku, int price) {
+		itemSKU = sku;
+		unitPrice = price;
+	}
+	public void setSpecial(int qty, int price) {
+		qtyForSpecial = qty;
+		priceForSpecial = price;
+		specialAvailable=true;
+	}
+	
+	public char getSKU() {
+		return itemSKU;
+	}
+	public int getPrice() {
+		return unitPrice;
+	}
+	public int getPriceForSpecial() {
+		return priceForSpecial;
+	}
+	public int getQtyForSpecial() {
+		return qtyForSpecial;
+	}
 }
+
