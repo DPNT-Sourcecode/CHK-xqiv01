5641f2a4a84005d7e4e9ea8d85aa1f6ca5ea9570
sbx_user1060 <sbx_user1060@ip-10-129-51-70.eu-west-2.compute.internal>
2019-01-15 15:40:00 +0000

    Tue Jan 15 15:40:00 UTC 2019

--- src/main/java/befaster/solutions/CHK/Basket.java
+++ src/main/java/befaster/solutions/CHK/Basket.java
@@ -1,6 +1,34 @@
 package befaster.solutions.CHK;
 
 public class Basket {
+	int totalA=0;
+	int totalB=0;
+	int totalC=0;
+	int totalD=0;
+	int totalE=0;
+	public void addItem(char sku) throws Exception {
+		switch (sku) {
+		case 'A':
+			totalA++;
+			break;
+		case 'B':
+			totalB++;
+			break;
+		case 'C':
+			totalC++;
+			break;
+		case 'D':
+			totalD++;
+			break;
+		case 'E':
+			totalE++;
+			break;
+		default:
+			throw new Exception();
+				
+		}
+	}
 	
 }
+
 
