1f4b51850ab08f6133f9f1eb51d08217016828b5
sbx_user1060 <sbx_user1060@ip-10-129-51-70.eu-west-2.compute.internal>
2019-01-15 16:08:59 +0000

    Tue Jan 15 16:08:59 UTC 2019

--- src/main/java/befaster/solutions/CHK/Basket.java
+++ src/main/java/befaster/solutions/CHK/Basket.java
@@ -46,13 +46,34 @@ public class Basket {
 	// calculations for the total cost for each item
 	public int checkTotals() {
 		int totalCost = 0;
-		
+		//check for specials of item A in multiples of 5
+		itemA.setSpecial(5, 200);
+		totalCost += itemA.checkSpecialCost(totalA);
+		totalA %= 5;
+		//check for specials of item A in multiples of 3
+		itemA.setSpecial(3, 130);
+		totalCost += itemA.checkSpecialCost(totalA);
+		totalA %= 3;
+		//check total cost of item A after specials applied
 		totalCost += itemA.checkCost(totalA);
+		// remove one free B item for every 2 E items
+		totalB -= totalE/2;
+		if (totalB <0) totalB=0;
+		//check for specials of item B in multiples of 2
+		itemB.setSpecial(2, 45);
+		totalCost += itemB.checkSpecialCost(totalB);
+		totalB %= 2;
+		//check total cost of item B after specials applied
 		totalCost += itemB.checkCost(totalB);
+		//check total cost of item C after specials applied
 		totalCost += itemC.checkCost(totalC);
+		//check total cost of item D after specials applied
 		totalCost += itemD.checkCost(totalD);
+		//check total cost of item E after specials applied
+		totalCost += itemE.checkCost(totalE);
 		
 		return totalCost;
 	}
 }
+
 
