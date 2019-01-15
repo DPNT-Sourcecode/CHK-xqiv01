14a694bada79d6246b47ebd71a8f979138c75b88
sbx_user1060 <sbx_user1060@ip-10-129-51-70.eu-west-2.compute.internal>
2019-01-15 16:04:00 +0000

    Tue Jan 15 16:04:00 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,50 +7,19 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
         //throw new SolutionNotImplementedException();
     	
-    	int totalCost = 0;
+    	
     	char [] items = skus.toCharArray();
-    	
-    	Item itemA = new Item('A',50);
-    	itemA.setSpecial(3, 130);
-    	Item itemB = new Item('B',30);
-    	itemB.setSpecial(2, 45);
-    	Item itemC = new Item('C',20);
-    	Item itemD = new Item('D',15);
-    	
-    	int totalA=0;
-    	int totalB=0;
-    	int totalC=0;
-    	int totalD=0;
-    	
+    	Basket allItems = new Basket();
+   	
     	// buying no items is no cost but shouldn't be considered an illegal input
     	if (skus.isEmpty()) return 0;
     	
     	try {
     		for (char item:items) {
-    			switch (item) {
-    			case 'A':
-    				totalA++;
-    				break;
-    			case 'B':
-    				totalB++;
-    				break;
-    			case 'C':
-    				totalC++;
-    				break;
-    			case 'D':
-    				totalD++;
-    				break;
-    			default:
-    				throw new Exception();
-    					
-    			}
+    			allItems.addItem(item); //add each character (item sku) to basket
     		}
-    		totalCost += itemA.checkCost(totalA);
-    		totalCost += itemB.checkCost(totalB);
-    		totalCost += itemC.checkCost(totalC);
-    		totalCost += itemD.checkCost(totalD);
     		
-    		return totalCost;
+    		return allItems.checkTotals();
     	}
     	catch (Exception e) {
     		return -1;
@@ -58,3 +27,4 @@ public class CheckoutSolution {
     	
     }
 }
+
