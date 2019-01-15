2a3dc973d2a77c5225133a69b6a0095cdaa54dbb
sbx_user1059 <sbx_user1059@ip-10-129-227-17.eu-west-2.compute.internal>
2019-01-15 17:25:51 +0000

    Tue Jan 15 17:25:51 UTC 2019

--- src/main/java/befaster/solutions/CHK/AvailableItems.java
+++ src/main/java/befaster/solutions/CHK/AvailableItems.java
@@ -3,35 +3,39 @@ package befaster.solutions.CHK;
 import java.util.HashMap;
 
 public class AvailableItems {
-	HashMap itemSet = new HashMap();
-	itemSet.put('A',new Item('A',50));
+	HashMap itemSet = new HashMap<>();
+	public AvailableItems() {
+		itemSet.put('A',new Item('A',50));
+		itemSet.put('B',new Item('B',30));
+		itemSet.put('C',new Item('C',20));
+		itemSet.put('D',new Item('D',15));
+		itemSet.put('E',new Item('E',40));
+		itemSet.put('F',new Item('F',10));
+		itemSet.put('G',new Item('G',20));
+		itemSet.put('H',new Item('H',10));
+		itemSet.put('I',new Item('I',35));
+		itemSet.put('J',new Item('J',60));
+		itemSet.put('K',new Item('K',80));
+		itemSet.put('L',new Item('L',90));
+		itemSet.put('M',new Item('M',15));
+		itemSet.put('N',new Item('N',40));
+		itemSet.put('O',new Item('O',10));
+		itemSet.put('P',new Item('P',50));
+		itemSet.put('Q',new Item('Q',50));
+		itemSet.put('R',new Item('R',50));
+		itemSet.put('S',new Item('S',50));
+		itemSet.put('T',new Item('T',50));
+		itemSet.put('U',new Item('U',50));
+		itemSet.put('V',new Item('V',50));
+		itemSet.put('W',new Item('W',50));
+		itemSet.put('X',new Item('X',50));
+		itemSet.put('Y',new Item('Y',50));
+		itemSet.put('Z',new Item('Z',50));
+	}
 	
-	Item itemA = new Item('A',50);
-	Item itemB = new Item('B',30);
-	Item itemC = new Item('C',20);
-	Item itemD = new Item('D',15);
-	Item itemE = new Item('E',40);
-	Item itemF = new Item('F',10);
-	Item itemG = new Item('G',20);
-	Item itemH = new Item('H',10);
-	Item itemI = new Item('I',35);
-	Item itemJ = new Item('J',60);
-	Item itemK = new Item('K',80);
-	Item itemL = new Item('L',90);
-	Item itemM = new Item('M',15);
-	Item itemN = new Item('N',40);
-	Item itemO = new Item('O',10);
-	Item itemP = new Item('P',50);
-	Item itemQ = new Item('Q',30);
-	Item itemR = new Item('R',50);
-	Item itemS = new Item('S',30);
-	Item itemT = new Item('T',20);
-	Item itemU = new Item('U',40);
-	Item itemV = new Item('V',50);
-	Item itemW = new Item('W',20);
-	Item itemX = new Item('X',90);
-	Item itemY = new Item('Y',10);
-	Item itemZ = new Item('Z',50);
-
+	public Item getValidItem (char sku) {
+		return (Item) itemSet.get(sku);
+	}
 }
+
 
